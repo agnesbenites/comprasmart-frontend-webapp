373fb8d882f342cff77adfa37fa1b98ae4ebb933
AgnesBenites <agnesbenites@gmail.com>
2025-11-09 21:58:10 -0300

    feat: atualizar link do CSV modelo no Supabase Storage

--- src/pages/LojistaDashboard/pages/LojistaProducts.jsx
+++ src/pages/LojistaDashboard/pages/LojistaProducts.jsx
@@ -868,7 +868,7 @@ const LojistaProdutos = () => {
           </p>
                    {" "}
           <a
-            href="https://vluxffbornrlxcepqmzr.supabase.co/storage/v1/object/public/planilha-produtos-modelo/planilha-produtos-modelo.csv"
+            href="https://vluxffbornrlxcepqmzr.supabase.co/storage/v1/object/public/planilha-produtos-modelo/PlanilhaProdutos.csv"
             target="_blank"
             rel="noopener noreferrer"
             style={{
