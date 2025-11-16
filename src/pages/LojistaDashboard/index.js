88cff162b95cca7fe14d9975d5645ea6593c5690
AgnesBenites <agnesbenites@gmail.com>
2025-11-15 22:04:01 -0300

    fix(index): Corrigindo exportação de componentes Lojista para Named Exports, resolvendo erro de carregamento.

--- src/pages/LojistaDashboard/index.js
+++ src/pages/LojistaDashboard/index.js
@@ -1,12 +1,19 @@
-﻿// Exportações principais do LojistaDashboard
-export { default as LojistaDashboard } from './pages/LojistaDashboard';
-export { default as LojistaEscolha } from './pages/LojistaEscolha';
-export { default as LojistaProducts } from './pages/LojistaProducts';
-export { default as LojistaUsuarios } from './pages/LojistaUsuarios';
-export { default as LojistaVendedores } from './pages/LojistaVendedores';
-export { default as LojistaFiliais } from './pages/LojistaFiliais';
-export { default as LojistaQRCode } from './pages/LojistaQRCode';
-export { default as LojistaCadastro } from './pages/LojistaCadastro';
-export { default as LojistaHomePanel } from './pages/LojistaHomePanel';
-export { default as LojistaPagamentos } from './pages/LojistaPagamentos';
-export { default as LojistaRelatorios } from './pages/LojistaRelatorios';
+// src/pages/LojistaDashboard/index.js
+
+// Re-exporta todos os componentes nomeados de cada arquivo .jsx
+// Este é o método mais robusto quando se usam Named Exports (export const Nome)
+
+export { LojistaDashboard } from "./pages/LojistaDashboard";
+export { LojistaEscolha } from "./pages/LojistaEscolha";
+export { LojistaFiliais } from "./pages/LojistaFiliais";
+export { LojistaHomePanel } from "./pages/LojistaHomePanel";
+export { LojistaPagamentos } from "./pages/LojistaPagamentos";
+export { LojistaProducts } from "./pages/LojistaProducts";
+export { LojistaQRCode } from "./pages/LojistaQRCode";
+export { LojistaRelatorios } from "./pages/LojistaRelatorios";
+export { LojistaUsuarios } from "./pages/LojistaUsuarios";
+export { LojistaVendedores } from "./pages/LojistaVendedores";
+
+// Se o seu LojistaDashboard.jsx também contiver os mocks de outros componentes,
+// certifique-se de que os arquivos individuais (como LojistaQRCode.jsx)
+// exportam algo, ou que o LojistaDashboard.jsx seja o único a exportá-los.
